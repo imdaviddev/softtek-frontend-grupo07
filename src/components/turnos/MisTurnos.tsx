63ac8658d2440440155313514a23d6c156366647
Dario Emanuel Diaz <dariodiaz91498@gmail.com>
2024-07-01 02:30:36 -0300

    envio de mail funcionando

--- src/components/turnos/MisTurnos.tsx
+++ src/components/turnos/MisTurnos.tsx
@@ -7,12 +7,14 @@ import Table from "@mui/joy/Table";
 import DownloadIcon from '@mui/icons-material/Download';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
+import SendIcon from '@mui/icons-material/Send';
 import RecetaModal from "./RecetaModal";
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import emailjs from 'emailjs-com';
 
 const MisTurnosTable = () => {
     const [rows, setRows] = useState<MisTurnosResponse[]>([]);
@@ -67,24 +69,48 @@ const MisTurnosTable = () => {
         setDialogOpen(true);
     };
 
+    const sendEmail = async (turno: MisTurnosResponse) => {
+        try {
+            const templateParams = {
+                to_email: "prueba@gmail.com", // Correo del usuario
+                subject: `Detalles del Turno N°${turno.id}`,
+                message: `
+                    Motivo de Consulta: ${turno.motivoConsulta}
+                    Fecha y Hora de la Cita: ${new Date(turno.fechaHoraCita).toLocaleString()}
+                    Nombre del Especialista: ${turno.especialista?.nombre}
+                `
+            };
+            await emailjs.send(
+                'service_13wchd7', // Service ID
+                'template_hzznazo', // Template ID
+                templateParams,
+                '2mU4JfYEJfiZI8udF' // User ID
+            );
+            alert('Correo enviado exitosamente!');
+        } catch (error) {
+            console.error('Error al enviar el correo:', error);
+            alert('Error al enviar el correo.');
+        }
+    };
+
     function ver_receta(receta: any) {
         setModalContent({
             title: 'Detalles de la Receta',
             text: ` ${receta.descripcion}`,
         });
         setModalOpen(true);
-    };
+    }
 
     return (
         <>
             <Table hoverRow>
                 <thead>
                     <tr>
-                        <th style={{ width: "15%" }}>Turno N°</th>
+                        <th style={{ width: "7%" }}>Turno N°</th>
                         <th>Motivo de Consulta</th>
                         <th>Fecha y Hora de la Cita</th>
                         <th>Nombre del especialista</th>
-                        <th style={{ width: "25%" }}>Acciones</th>
+                        <th style={{ width: "30%" }}>Acciones</th>
                         <th>Receta</th>
                     </tr>
                 </thead>
@@ -96,22 +122,32 @@ const MisTurnosTable = () => {
                             <td>{new Date(row.fechaHoraCita).toLocaleString()}</td>
                             <td>{row.especialista?.nombre}</td>
                             <td>
-                                <ButtonGroup spacing="0.9rem" aria-label="spacing button group">
-                                    <Button variant="outlined" startIcon={<EditIcon />} onClick={function () { }} >Editar</Button>
-                                    <Button variant="outlined" startIcon={<DeleteForeverIcon />} onClick={() => handleEliminarClick(row.id)}>Eliminar</Button>
+                                <ButtonGroup spacing="0.2rem" aria-label="spacing button group" >
+                                    
+                                    <Button size="small" variant="outlined" startIcon={<EditIcon />} onClick={function () { }}>
+                                        Editar
+                                    </Button>
+                                    
+                                    <Button size="small" variant="outlined" startIcon={<DeleteForeverIcon />} onClick={() => handleEliminarClick(row.id)}>
+                                        Eliminar
+                                    </Button>
+                                    
+                                    <Button size="small" variant="outlined" startIcon={<SendIcon />} onClick={() => sendEmail(row)}>
+                                        Enviar Correo
+                                    </Button>
+
                                 </ButtonGroup>
                             </td>
                             <td>
                                 {row.receta ? (
-                                    <Button onClick={() => ver_receta(row.receta)} variant="outlined" startIcon={<DownloadIcon />}>
+                                    <Button size="small" variant="outlined" startIcon={<DownloadIcon />} onClick={() => ver_receta(row.receta)} >
                                         Disponible
                                     </Button>
                                 ) : (
-                                    <Button disabled variant="outlined">
+                                    <Button size="small" variant="outlined" disabled>
                                         Aun no cargada
                                     </Button>
                                 )}
-
                             </td>
                         </tr>
                     ))}
